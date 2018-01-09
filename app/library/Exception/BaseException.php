42ee68704d1216bcd5f9aaf39ac8a0000a1b82d4
fallen <haiyan.xu.vip@gmail.com>
2018-01-09 16:23:10 +0800

    *

--- app/library/Exception/BaseException.php
+++ app/library/Exception/BaseException.php
@@ -1,18 +1,38 @@
 <?php
 namespace Exception;
 
-class BaseException extends \Exception
+use Throwable;
+
+class BaseException implements Throwable
 {
     public $code    = 0;
     public $message = '';
     public $data    = [];
     public $url     = '';
 
-    public function __construct($message = "", $code = 500, $data = [], $url = '')
-    {
-        $this->code     = $code;
-        $this->message  = !empty($message)? $message: '';
-        $this->data     = !empty($data)? $data: [];
-        $this->url      = !empty($url)? $url: '';
-    }
+//    public function __construct($message = "", $code = 500, $data = [], $url = '')
+//    {
+//        $this->code     = $code;
+//        $this->message  = !empty($message)? $message: '';
+//        $this->data     = !empty($data)? $data: [];
+//        $this->url      = !empty($url)? $url: '';
+//    }
+
+    public function __construct($message = "", $code = 0, Throwable $previous = null) { }
+
+    public function getMessage(){}
+
+    public function getCode(){}
+
+    public function getFile(){}
+
+    public  function getLine(){}
+
+    public function getTrace(){}
+
+    public function getTraceAsString(){}
+
+    public function getPrevious(){}
+
+    public function __toString(){}
 }
--- app/library/Exception/JsonErrorException.php
+++ app/library/Exception/JsonErrorException.php
@@ -3,8 +3,4 @@ namespace Exception;
 
 class JsonErrorException extends BaseException
 {
-    public function __construct($message = "", $code = 500, array $data = [], $url = '')
-    {
-        parent::__construct($message, $code, $data, $url);
-    }
 }
